8920e3c5527290e7ce3342fbccc3daf8fdfd07b5
Jonathan Gluck <jonathangluck08854@gmail.com>
2013-11-11 13:57:57 -0500

    added 3 testcases and cleanup files

--- testcases/tc_single_break_high_coverage/run_test.sh
+++ testcases/tc_single_break_high_coverage/run_test.sh
@@ -4,13 +4,14 @@
 python ../../errorgenerator/errorgen.py -a ../../data/buchnera/3000/buchnera-udp.assembly.fasta -o singleErrorAssembly.fasta -m singleErrorMetadata.txt -e 2000 -l 200
 
 #Generate 5x read coverage over error assembly
-wgsim -1 300 -2 300 -R 0.0 -X 0.0 -e 0.0 -N 50 singleErrorAssembly.fasta single.1.fastq single.2.fastq
+wgsim -1 300 -2 300 -R 0.0 -X 0.0 -e 0.0 -N 100 singleErrorAssembly.fasta single.1.fastq single.2.fastq
 
 #Convert fastq to fasta
 python ../../errorgenerator/convertFASTQ.py single.1.fastq singleReads.1.fasta
 
 rm -f single.1.fastq 
 rm -f single.2.fastq
+
 
 python ../../breakpoint-detection/generate_unaligned.py  ../../data/buchnera/3000/buchnera-udp.assembly.fasta singleReads.1.fasta singleUnaligned.txt
 
