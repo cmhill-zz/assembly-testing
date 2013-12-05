0481a9a118254bf9c1c12098f3eb863d99a71cd6
bharatsingh430 <bharatsingh430@gmail.com>
2013-12-05 14:52:39 -0500

    last commit did not work
    this should fix the previous problem

--- testcases/tc_gau_ins_del/eval_gau_ins_del.py
+++ testcases/tc_gau_ins_del/eval_gau_ins_del.py
@@ -20,13 +20,13 @@ accuracyLocation = 'result.txt'
 #genAsblyAndOrcl.generateValidation(orgSq, ipSq, oracleLocation, True, True)
 
 #generate reads
-os.system('wgsim -1 40 -2 40 -R 0.0 -X 0.0 -e 0.0 -N 10000 -d 200 -s 0 ' + orgSq + ' ' + read1 + ' ' + read2 + ' >/dev/null')
+#os.system('wgsim -1 40 -2 40 -R 0.0 -X 0.0 -e 0.0 -N 10000 -d 200 -s 0 ' + orgSq + ' ' + read1 + ' ' + read2 + ' >/dev/null')
 
 #build index
-os.system('bowtie2-build ' + ipSq + ' ' + outputIndex + ' >/dev/null')
+#os.system('bowtie2-build ' + ipSq + ' ' + outputIndex + ' >/dev/null')
 
 #align reads
-os.system('bowtie2 -x ' + outputIndex + ' -1 ' + read1 + ' -2 ' + read2 + ' -S ' + outputMatchBowtie + ' >/dev/null')
+#os.system('bowtie2 -x ' + outputIndex + ' -1 ' + read1 + ' -2 ' + read2 + ' -S ' + outputMatchBowtie + ' >/dev/null')
 
 #evaluate the alignment using mate pair criterion
 gaussianCheck.gCk(outputMatchBowtie, matchGaussian, 40)
