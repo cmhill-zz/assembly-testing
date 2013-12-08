d7959549d60acb8e134bc2ac4228e3dfe9a279b6
yunusb <yunusb@cs.umd.edu>
2013-12-08 18:38:45 -0500

    Test cases are formed to clear their garbage

--- testcases/tc_gmb_3/run_test.sh
+++ testcases/tc_gmb_3/run_test.sh
@@ -1,7 +1,6 @@
 #! /usr/bin/sh
 
 python ../../src/matePairAnalysis.py --fasta lambda_virus_modified.fa -1 ../../data/lambda_virus/reads_1.fq -2 ../../data/lambda_virus/reads_2.fq  --gmb > result
-cat result
 rm *.sam *.bam *.bai &> /dev/null
 python ../../src/tc_gmb_oracle.py --oracle oracle --result result
 
