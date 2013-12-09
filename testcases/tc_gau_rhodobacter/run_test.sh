057683258ff884442e43be9a8a86149820c1262c
yunusb <yunusb@cs.umd.edu>
2013-12-09 15:44:49 -0500

    run tests are modified

--- testcases/tc_gau_rhodobacter/run_test.sh
+++ testcases/tc_gau_rhodobacter/run_test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 
-python ../../src/matePairAnalysis.py --fasta ../../data/rhodobacter/rhodobacterMod.assembly.fasta -1 ../../data/rhodobacter/rhodobacter.sequences.1.fasta -2 ../../data/rhodobacter/rhodobacter.sequences.2.fasta --gau
-rm *.sam *.bam *.bai
+python ../../src/matePairAnalysis.py --fasta rhodobacterMod.assembly.fasta -1 rhodobacter.sequences.1.fasta -2 rhodobacter.sequences.2.fasta --gau > match.txt
+rm *.sam *.bam *.bai &> /dev/null
 #python eval_gau_del.py
 
 exit $?
