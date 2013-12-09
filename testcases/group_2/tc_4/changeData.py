aa4ec5962fc5a845aeadfad542c512c334bd648d
bharatsingh430 <bharatsingh430@gmail.com>
2013-12-09 18:04:40 -0500

    updated test cases and added oracle for rhodobacter

--- testcases/group_2/tc_4/changeData.py
+++ testcases/group_2/tc_4/changeData.py
@@ -1,11 +1,11 @@
-f = open('oracle.txt', 'r')
-out = open('oracle', 'w')
+f = open('oracle', 'r')
+out = open('oracle.txt', 'w')
 
 for lines in f:
     data = lines.split('\t')
     contig = data[0]
     left = int(data[1])
-    right = int(data[2]) + left
+    right = int(data[2]) - left
     error = data[3]
     out.write(contig + '\t' + str(left) + '\t' + str(right) + '\t' + error)
 
