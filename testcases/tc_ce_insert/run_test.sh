b05296e9345b4967c3522c209ed0d21e2530ae69
andrewpachulski <ajp@cs.umd.edu>
2013-12-09 15:08:58 -0500

    Update run_test.sh

--- testcases/tc_ce_insert/run_test.sh
+++ testcases/tc_ce_insert/run_test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 python ../../src/matePairAnalysis.py --ce --fasta insTest.fasta -1 r1.fq -2 r2.fq --ce_threshold 1.2 --ce_windowsize 150 --ce_windowstep 100 > results.txt
+rm *.bam
+rm *.sam
+rm *.bai
+python ../../src/ceOracle.py results.txt 1
 
-exit 0
+
+exit $?
