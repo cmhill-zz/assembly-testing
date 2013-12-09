26673d3a924560cc9c7e81cb315589a9e5492d93
yunusb <yunusb@cs.umd.edu>
2013-12-09 18:43:19 -0500

    test is updated.

--- testcases/group_2/tc_9/run_test.sh
+++ testcases/group_2/tc_9/run_test.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/sh
 
-python ../../src/matePairAnalysis.py --fasta lambda_virus_modified.fa -1 reads_1.fq -2 reads_2.fq  --gmb > result
+python ../../../src/matePairAnalysis.py --fasta lambda_virus_modified.fa -1 reads_1.fq -2 reads_2.fq  --gmb > result
 rm *.sam *.bam *.bai &> /dev/null
-python ../../src/tc_gmb_oracle.py --oracle oracle --result result
+python ../../../src/tc_gmb_oracle.py --oracle oracle --result result
 
 exit $?
