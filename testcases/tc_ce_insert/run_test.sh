4d49bba9514628409ce083e12fab0346124f39ba
andrewpachulski <ajp@cs.umd.edu>
2013-12-09 14:29:17 -0500

    Update run_test.sh
    fixed threshold

--- testcases/tc_ce_insert/run_test.sh
+++ testcases/tc_ce_insert/run_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python ../../src/matePairAnalysis.py --ce --fasta insTest.fasta -1 r1.fq -2 r2.fq --ce_theshold 1.2 --ce_windowsize 150 --ce_windowstep 100 > results.txt
+python ../../src/matePairAnalysis.py --ce --fasta insTest.fasta -1 r1.fq -2 r2.fq --ce_threshold 1.2 --ce_windowsize 150 --ce_windowstep 100 > results.txt
 
 exit 0
