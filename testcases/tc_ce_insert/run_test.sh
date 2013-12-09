c881f700d32dbf3a2ca8f46d106d282e9e17b153
andrewpachulski <ajp@cs.umd.edu>
2013-12-09 14:27:09 -0500

    Update run_test.sh
    corrected for new entry method

--- testcases/tc_ce_insert/run_test.sh
+++ testcases/tc_ce_insert/run_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python ../../src/ceOracle.py ./output.sam 100 150 1
+python ../../src/matePairAnalysis.py --ce --fasta insTest.fasta -1 r1.fq -2 r2.fq --ce_theshold 1.2 --ce_windowsize 150 --ce_windowstep 100 > results.txt
 
 exit 0
