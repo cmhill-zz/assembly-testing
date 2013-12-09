8f922bafcaba0a83cca0768bdab7a99eada316fc
yunusb <yunusb@cs.umd.edu>
2013-12-09 14:47:18 -0500

    Good minus bad test cases setup correctly.

--- testcases/tc_gmb_1/run_test.sh
+++ testcases/tc_gmb_1/run_test.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/sh
 
-python ../../src/matePairAnalysis.py --fasta lambda_virus_modified.fa -1 ../../data/lambda_virus/reads_1.fq -2 ../../data/lambda_virus/reads_2.fq  --gmb > result
+python ../../src/matePairAnalysis.py --fasta lambda_virus_modified.fa -1 reads_1.fq -2 reads_2.fq  --gmb > result
 rm *.sam *.bam *.bai &> /dev/null
 python ../../src/tc_gmb_oracle.py --oracle oracle --result result
 
