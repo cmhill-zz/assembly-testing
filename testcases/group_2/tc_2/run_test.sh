4adb6cf41d32ec0dcfbca74b47d8efc026fc8db1
bharatsingh430 <bharatsingh430@gmail.com>
2013-12-09 18:38:18 -0500

    updated scripts

--- testcases/group_2/tc_2/run_test.sh
+++ testcases/group_2/tc_2/run_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python ../../../src/matePairAnalysis.py --ce --fasta insTest.fasta -1 r1.fq -2 r2.fq --ce_threshold 1.2 --ce_windowsize 150 --ce_windowstep 100 > match.txt
+python ../../../src/matePairAnalysis.py --ce --fasta insTest.fasta -1 r1.fq -2 r2.fq --ce_threshold 2.5 --ce_windowsize 150 --ce_windowstep 100 > match.txt
 rm *.bam *.sam *.bai &> /dev/null
 python eval_ce_ins.py
 
