4f7f14d098b78722055c3dbed854e58c02a75ecc
bharatsingh430 <bharatsingh430@gmail.com>
2013-12-09 17:38:44 -0500

    updating rhodobacter

--- testcases/group_2/tc_6/run_test.sh
+++ testcases/group_2/tc_6/run_test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-python ../../src/matePairAnalysis.py --fasta rhodobacterMod.assembly.fasta -1 rhodobacter.sequences.1.fasta -2 rhodobacter.sequences.2.fasta --gau > match.txt
+python ../../../src/matePairAnalysis.py --fasta rhodobacterMod.assembly.fasta -1 rhodobacter.sequences.1.fasta -2 rhodobacter.sequences.2.fasta --gau > match.txt
 rm *.sam *.bam *.bai &> /dev/null
 #python eval_gau_del.py
 
