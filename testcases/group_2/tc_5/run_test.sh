7939a704ba83fd033ca14765d69ad220f0f75dde
bharatsingh430 <bharatsingh430@gmail.com>
2013-12-09 23:38:00 -0500

    updated test cases so that the arguments are in proper order!!

--- testcases/group_2/tc_5/run_test.sh
+++ testcases/group_2/tc_5/run_test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
-python ../../../src/matePairAnalysis.py --fasta insDelTest.fasta -1 r1.fq -2 r2.fq --gau > match.txt
+python ../../../src/matePairAnalysis.py --gau --fasta insDelTest.fasta -1 r1.fq -2 r2.fq > match.txt
 rm *.sam *.bam *.bai &> /dev/null
 python eval_gau_ins_del.py
 
